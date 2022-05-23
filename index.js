59e1c10e76d6f70d4871dbde6798f26eaf2b2fe1
datdang0899 <105406960+datdang0899@users.noreply.github.com>
2022-05-23 15:36:06 +0700

    co tinh spam .

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 console.log("hello cac ban minh la Dat dep trai day .");
 
-console.log("hehe mình cố ý commit spam đấy ,  thì sao nào . ") ; 
+console.log("hehe mình cố ý commit spam đấy ,  thì sao nào . , lai la co tinh day ne") ; 
